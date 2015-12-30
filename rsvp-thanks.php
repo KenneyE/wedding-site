cb05fce4c620118548a61b9040e53f96398b9ff5
Eric Kenney <ekenney5@gmail.com>
2015-12-30 10:58:11 -0700

    Initial commit

---
+++ rsvp-thanks.php
@@ -0,0 +1,287 @@
+<!DOCTYPE HTML>
+<!--
+  Big Picture by HTML5 UP
+  html5up.net | @n33co
+  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+  <head>
+    <title>Eric Loves Sarah</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+  </head>
+  <body>
+
+    <!-- Header -->
+      <header id="header">
+
+        <!-- Logo -->
+          <h1 id="logo"><a href="index.html">Eric &amp; Sarah</a></h1>
+
+        <!-- Nav -->
+          <nav id="nav">
+            <ul>
+              <li><a href="#intro">Eric &amp; Sarah</a></li>
+              <li><a href="#one">About Us</a></li>
+              <li><a href="#two">The Venue</a></li>
+              <li><a href="#three">Lodging</a></li>
+              <li><a href="#four">CB Activities</a></li>
+              <li><a href="#five">Registries</a></li>
+              <li><a href="#work">Gallery</a></li>
+              <li><a href="#rsvp">RSVP</a></li>
+            </ul>
+          </nav>
+
+      </header>
+
+    <!-- Intro -->
+      <section id="intro" class="main style1 dark fullscreen">
+        <div class="content container 75%">
+          <header>
+            <!-- <h2>Eric &amp; Sarah</h2> -->
+            <?php
+            // Change this to YOUR address
+            $recipient = 'ekenney5@gmail.com';
+            $name = $_POST['name'];
+            $attending= $_POST['attending'];
+            $food = $_POST['food'];
+            $notes = $_POST['notes'];
+            $email = "ekenney5@gmail.com";
+
+            $subject = "RSVP from EricLovesSarah.com!";
+            $body = "Names: " . $name . "\r\n\r\n" .
+              "Attending: " . $attending . "\r\n\r\n" .
+                "Food Options: " . $food . "\r\n\r\n" .
+                  "Notes: " . $notes;
+
+
+            # We'll make a list of error messages in an array
+            $messages = array();
+            # CAREFUL: don't allow hackers to sneak line breaks and additional
+            # headers into the message and trick us into spamming for them!
+            $subject = preg_replace('/\s+/', ' ', $subject);
+            # Make sure the subject isn't blank afterwards!
+            if (preg_match('/^\s*$/', $name)) {
+              $messages[] = "Please specify a name for your RSVP.";
+            }
+            # Make sure the message has a body
+            if (preg_match('/^\s*$/', $attending)) {
+              $messages[] = "You didn't tell us if you're attending!";
+            }
+            if (preg_match('/^\s*$/', $food)) {
+              $messages[] = "You didn't tell us what you're eating!";
+            }
+            if (count($messages)) {
+              $subject = "INCOMPLETE RSVP from EricLovesSarah.com";
+              mail($recipient,
+              $subject,
+              $body,
+              "From: Wedding RSVP <$email>\r\n");
+
+              # There were problems, so tell the user and
+              # don't send the message yet
+              echo("<h2>WAIT!</h2><h2>You missed something!</h2>");
+              foreach ($messages as $message) {
+                echo("<h3>$message</h3>\n");
+              }
+              echo("<h3>Please fix these problems and RSVP one more time.</h3>");
+            } else {
+              # Send the email - we're done
+              mail($recipient,
+              $subject,
+              $body,
+              "From: Wedding RSVP <$email>\r\n");
+              echo("<h2>Thanks for the RSVP!</h2>");
+            }
+            ?>
+          </header>
+          <p>We're getting married!</p>
+          <footer>
+            <a href="#one" class="button style2 down">More</a>
+          </footer>
+        </div>
+      </section>
+
+    <!-- One -->
+      <section id="one" class="main style2 right dark fullscreen">
+        <div class="content box style2">
+          <header>
+            <h2>About Us</h2>
+          </header>
+          <p>We met at the University of Colorado in Boulder in 2008. Less than a year later we started dating and now here we are! Now we live in Denver with our pup Blue.</p>
+        </div>
+        <a href="#two" class="button style2 down anchored">Next</a>
+      </section>
+
+    <!-- Two -->
+      <section id="two" class="main style2 right dark fullscreen">
+        <div class="content box style2">
+          <header>
+            <h2>The Venue</h2>
+          </header>
+          <p>We're getting married at the Ten Peaks Event Site at Crested Butte Mountain Resort. For guests staying in the town of Crested Butte or at the base of the mountain, shuttles will be running throughout the evening to run you up the hill. They will be stopping at HERE and HERE between TIME and TIME. Please be on a shuttle no later than TIME to be on time for the ceremony.</p>
+        </div>
+        <a href="#three" class="button style2 down anchored">Next</a>
+      </section>
+
+    <!-- Three -->
+      <section id="three" class="main style2 right dark fullscreen">
+        <div class="content box style2">
+            <h2>Lodging</h2>
+          </header>
+          <p>Guests will receive <strong>20&#37;</strong> off rooms at Crested Butte Mountain Resorts if booked before <strong>July 13, 2016</strong>.
+              <br />
+              To see lodging options, go to: <strong><a class="light" href="http://www.skicb.com/lodging" target="_blank">http://www.skicb.com/lodging</a></strong><br />
+        Every guest that stays with the resort gets a free lift ticket!
+    </p>
+    <p>
+          <strong>To Book:</strong><br />
+          Call: <strong><a href="tel:8443742148" class="light" target="_blank">(844) 374-2148</a></strong> between the hours of 8am-6pm Mountain Time<br />
+          Email: <strong><a href="mailto:info@cbmr.com" class="light" target="_blank">info@cbmr.com</a></strong><br />
+          Online: <strong><a href="http://www.skicb.com/mygroup" class="light" target="_blank">http://www.skicb.com/mygroup</a></strong> Package Code: <strong>9051</strong><br />
+          </p>
+        </div>
+        <a href="#four" class="button style2 down anchored">Next</a>
+      </section>
+
+    <!-- Four -->
+      <section id="four" class="main style2 left dark fullscreen">
+        <div class="content box style2">
+          <header>
+            <h2>Crested Butte Activities</h2>
+          </header>
+          <p>There's so much stuff to do in Crested Butte! You can go shopping on Elk Avenue, go on a hike, rent some mountain bikes, go fishing or just ride a chairlift up and down. Here's some more ideas: <strong><a href="http://www.skicb.com/summer-activities" class="light" target="_blank">http://www.skicb.com/summer-activities</a></strong></p>
+        </div>
+        <a href="#five" class="button style2 down anchored">Next</a>
+      </section>
+
+    <!-- Five -->
+      <section id="five" class="main style2 left dark fullscreen">
+        <div class="content box style2">
+          <header>
+            <h2>Registries</h2>
+          </header>
+          <!-- <p>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
+          Fusce blandit ultrices sapien, in accumsan orci rhoncus eu. Sed sodales venenatis arcu,
+          id varius justo euismod in. Curabitur egestas consectetur magna.</p> -->
+              <ul>
+                  <li><a class="light"href="http://www.rei.com/GiftRegistryDetails/GR124738834">REI</a></li>
+                  <li>Crate &amp; Barrel</li>
+              </ul>
+        </div>
+        <a href="#work" class="button style2 down anchored">Next</a>
+      </section>
+
+    <!-- Work -->
+      <section id="work" class="main style3 primary">
+        <div class="content container">
+          <header>
+            <h2>Gallery</h2>
+            <!-- <p>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
+            Fusce blandit ultrices sapien, in accumsan orci rhoncus eu. Sed sodales venenatis
+            arcu, id varius justo euismod in. Curabitur egestas consectetur magna vitae.</p> -->
+          </header>
+
+          <!-- Lightbox Gallery  -->
+            <div class="container 75% gallery">
+              <div class="row 0% images">
+                <div class="6u 12u(mobile)"><a href="images/fulls/mcconaughy.jpg" class="image fit from-left"><img src="images/thumbs/mcconaughy.jpg" title="Lake McConaughy" alt="" /></a></div>
+                <div class="6u 12u(mobile)"><a href="images/fulls/thailand.jpg" class="image fit from-right"><img src="images/thumbs/thailand.jpg" title="Thailand" alt="" /></a></div>
+              </div>
+              <div class="row 0% images">
+                <div class="6u 12u(mobile)"><a href="images/fulls/hawaii1.jpg" class="image fit from-left"><img src="images/thumbs/hawaii1.jpg" title="Hawaii" alt="" /></a></div>
+                <div class="6u 12u(mobile)"><a href="images/fulls/balloon_fiesta.jpg" class="image fit from-right"><img src="images/thumbs/balloon_fiesta.jpg" title="Balloon Fiesta" alt="" /></a></div>
+              </div>
+              <div class="row 0% images">
+                <div class="6u 12u(mobile)"><a href="images/fulls/thailand2.jpg" class="image fit from-left"><img src="images/thumbs/thailand2.jpg" title="Bangkok" alt="" /></a></div>
+                <div class="6u 12u(mobile)"><a href="images/fulls/phiphi.jpg" class="image fit from-right"><img src="images/thumbs/phiphi.jpg" title="Kho Phi Phi" alt="" /></a></div>
+              </div>
+            </div>
+
+        </div>
+      </section>
+
+    <!-- RSVP -->
+      <section id="rsvp" class="main style3 secondary">
+        <div class="content container">
+          <header>
+            <h2>RSVP</h2>
+            <p>Please let us know by SOME DATE HERE whether or not you'll be able to make it.</p>
+          </header>
+          <div class="box container 75%">
+
+          <!-- RSVP Form -->
+              <form method="POST" action="rsvp-thanks.php">
+                <div class="row 50%">
+                  <div class="12u 12u(mobile)"><input type="text" name="name" placeholder="Name" /></div>
+                  <div class="12u 12u(mobile)">
+                      <h4>Will You be Attending?</h4>
+                      <input type="radio" name="attending" value="Saturday Ceremony &amp; Reception and Welcome Dinner"/>Ceremony &amp; Reception and Friday Welcome Dinner<br>
+                      <input type="radio" name="attending" value="Saturday Ceremony &amp; Reception Only" />Ceremony &amp; Reception Only<br>
+                      <input type="radio" name="attending" value="Will not be attending" />Will not be able to attend<br>
+                  </div>
+
+                  <div class="12u 12u(mobile)">
+                      <h4>Whatcha Eatin'?</h4>
+                      <input type="radio" name="food" value="Steak"/>Steak<br>
+                      <input type="radio" name="food" value="Fish" />Fish<br>
+                      <input type="radio" name="food" value="Veggie" />Vegeterian<br>
+                  </div>
+                </div>
+                <div class="row 50%">
+                  <div class="12u"><textarea name="notes" placeholder="Anything Else You Want to Tell Us?" rows="6"></textarea></div>
+                </div>
+                <div class="row">
+                  <div class="12u">
+                    <ul class="actions">
+                      <li><input type="submit" value="Send Message" /></li>
+                    </ul>
+                  </div>
+                </div>
+              </form>
+
+          </div>
+        </div>
+      </section>
+
+    <!-- Footer -->
+      <footer id="footer">
+
+        <!-- Icons -->
+          <ul class="actions">
+            <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+          </ul>
+
+        <!-- Menu -->
+          <ul class="menu">
+              <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+          </ul>
+
+      </footer>
+
+    <!-- Scripts -->
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/jquery.poptrox.min.js"></script>
+      <script src="assets/js/jquery.scrolly.min.js"></script>
+      <script src="assets/js/jquery.scrollex.min.js"></script>
+      <script src="assets/js/skel.min.js"></script>
+      <script src="assets/js/util.js"></script>
+      <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+      <script src="assets/js/main.js"></script>
+
+      <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-35811349-4', 'auto');
+      ga('send', 'pageview');
+
+      </script>
+  </body>
+</html>
